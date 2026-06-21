8a42fc0bc0c75969ede9e33450751e7ad01cccc4
Tanner Jr., David Arthur <dtannerjr@bwh.harvard.edu>
2026-06-21 11:48:07 -0400

    signup wording

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -64,10 +64,10 @@ export default function Landing() {
 
           <div className="flex flex-col sm:flex-row gap-4 justify-center">
             <a href="/signup" className="btn btn-primary btn-lg">
-              Get my UUID
+              Sign up
             </a>
             <a href="/login" className="btn btn-outline btn-lg">
-              I already have one
+              Sign in
             </a>
           </div>
 
